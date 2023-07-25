83a8e2984f4f64da627b25726513059cb2d9dc2a
BenadrylWaifu <140027952+BenadrylWaifu@users.noreply.github.com>
2023-07-25 06:56:43 +0200

    main update 1
    yes

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 var song = document.getElementById("song"); 
 document.getElementById("video").setAttribute("autoplay", "autoplay");
 
-song.volume = 0.25; 
+song.volume = 0.15; 
 
-function start() {
-    document.getElementById("start").style.display = "none";
-    document.getElementById("content").style.display = "block";
-    song.play(); 
-}
+
+song.play()
